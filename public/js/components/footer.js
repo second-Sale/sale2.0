42fc8db4a462c3c79bad718c8f34f05e37c61380
lyf <2531349593@qq.com>
2017-05-07 15:33:26 +0800

    add footer

---
+++ public/js/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return <div>
+            <div className="footerStyle">
+                <div className="footer">
+                    <span className="footer1">关于我们</span>
+                    <span className="footer2">售后服务</span>
+                    <span className="footer3">业务合作</span>
+                    <span className="footer4">联系我们</span>
+                </div>
+                <div className="footer8">
+                    <span className="footer5">联系方式：15632896542</span>
+                    <span className="footer6">qq：254782639</span>
+                    <span className="footer7">公众号：second-sale</span>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Footer;
