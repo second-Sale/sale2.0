a4c26f49d71257b88142d4d0131e2d24675d148c
lyf <2531349593@qq.com>
2017-05-07 14:27:47 +0800

    update nav style

--- public/js/components/nav.js
+++ public/js/components/nav.js
@@ -40,20 +40,19 @@ class Nav extends React.Component {
 
     render() {
         return <div id="navStyle">
+
             <span onClick={this.Home.bind(this)} className="homeButton">首页</span>
             <span onClick={this.Publish.bind(this)} className="publishButton">发布信息</span>
             <span onClick={this.Person.bind(this)} className="personButton">个人中心</span>
 
-            <div>
-                <div className={this.props.isLogin === false ? "" : "hidden" } id="navButton">
-                    <span onClick={this.Login.bind(this)} className="">登录</span>
-                    <span onClick={this.Sign.bind(this)} className="navButton2">注册</span>
-                </div>
+            <div className={this.props.isLogin === false ? "" : "hidden" } id="navButton">
+                <span onClick={this.Login.bind(this)} className="">登录</span>
+                <span onClick={this.Sign.bind(this)} className="navButton2">注册</span>
+            </div>
 
-                <div className={this.props.isLogin === false ? "hidden" : "" } id="navButton">
-                    <span className="navButton3">{this.props.user},你好！</span>
-                    <span onClick={this.LoginOut.bind(this)} className="navButton2">登出</span>
-                </div>
+            <div className={this.props.isLogin === false ? "hidden" : "" } id="navButton">
+                <span className="navButton3">{this.props.user},你好！</span>
+                <span onClick={this.LoginOut.bind(this)} className="navButton2">登出</span>
             </div>
         </div>
     }
